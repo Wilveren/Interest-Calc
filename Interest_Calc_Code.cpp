7f67d1d1c2acdb511378ea009e4f4b3dc05ce0ba
William McLean <w.mclean1986@gmail.com>
2019-12-04 17:24:30 -0500

    Version 2.0
    - Added Input Data Cleanup function to remove spaces or commas, allowing easier usability
    - Added additional formatting functions so output displays as currency with correct commas and decimal places
    - Removed unneeded variable for greater simplicity
    - Updated comments

--- Interest_Calc_Code.cpp
+++ Interest_Calc_Code.cpp
@@ -4,16 +4,18 @@
 // of money deposited by a user.
 
 #include <iostream>
+#include <iomanip>
+#include <string>
 using namespace std;
 
-float CalcIntMonth(float &balance, float monthDeposit, float Rate){ //Function to calculate earned interest, and update balance
-float EarnedInt=0; //initializing variable for Total Interest Earned
-float monthRate = Rate / 12.0; //convert yearly interest rate to monthly interest rate
+float CalcIntMonth(float &balance, float monthDeposit, float yearRate){ //Function to calculate earned interest, and update balance
+float EarnedInt = 0; //initializing variable for Total Interest Earned
+float monthRate = yearRate / 12.0; //convert yearly interest rate to monthly interest rate and store in new variable
 
 EarnedInt = balance * monthRate; //Calculating interest earned in first month
 balance = balance + (balance * monthRate); //Updating balance with interest earned in first month
 
-for (unsigned int i=1;i<12;i++){ //Loop will run 11 times, one for each month of the year after the first.
+for (unsigned int i=1; i<12; i++){ //Loop will run 11 times, one for each month of the year after the first.
 //Loop will calculate the new balance after the user's monthly deposit, increase the amount of
 //Total Interest Earned by calculating Interest Earned for the month using the new balance, 
 //and update the balance by adding interest earned for the month
@@ -24,39 +26,104 @@ balance = balance + (balance * monthRate);
 return EarnedInt; //Returning the Total Interest Earned for the month.
 }
 
+string RemoveGarbage (string userString){ // Function for removing commas and spaces from user input
+
+   
+    for (unsigned int i=0;i<userString.size();++i){
+      if (userString.at(i)==' '){
+         userString.erase(i,1);
+         i=i-1;
+      }
+    }
+	
+	for (unsigned int i=0;i<userString.size();++i){
+      if (userString.at(i)==','){
+         userString.erase(i,1);
+         i=i-1;
+      }
+    }
+	
+    return userString;
+}
+
+void FormatCurrency(string &output){ //Function for placing a comma where necessary, when the value is 1,000 or greater
+	int decimal = output.find('.');
+	
+	if (decimal > 3){
+		for (int i = (decimal - 3); i > 0; i -= 3){
+			output.insert(i, ",");
+		}
+	}
+return;	
+}
+
+string Rounding(string output){ //Function for formatting output to have two decimal places
+	int decimal = output.find('.');
+	int x = decimal+3;
+	while (decimal+3<output.size())
+	{
+		output.erase(x);
+	}
+	return output;
+}
+
+
 int main(){
 	
-	float fDeposit; //Variable for initial deposit by customer
-	float balance; //Variable for balance of the account, will be passed by reference to function
-	float monthDeposit; //Variable for deposit customer will make each month
-	float rate; //Variable for yarly interest rate converted to a decimal
-	float rawRate; //Variable for interest rate given by customer as a percent
-	float interestGain; //Variable for total amount of interest gained over the year
 	
-	cout << "Please enter the amount of the initial deposit. Do not use commas. Ex: $1,000 should be entered as '1000'." << endl;
-	cin >> fDeposit;
-	cout << "How much will you deposit per month?" << endl;
-	cin >> monthDeposit;
+	float fDeposit; //Variable for initial deposit by customer
+	float balance; //Variable for balance of the account, will be passed by reference to function for calculation of growth
+	float monthDeposit; //Variable for deposit customer will make each month
+	float rate; //Variable for yearly interest rate, will be converted to a decimal
+	float interestGain; //Variable for total amount of interest gained over the year
+	string input; //Variable for user input. Will be processed to remove commas and extra spaces, then fed to fDeposit and monthDeposit
+	string output; //Variable for output. Float will be converted to String to allow for formatting as currency.
+	
+	//Ask user for input, then remove any commas or spaces from it using function RemoveGarbage
+	cout << "Please enter the amount of the initial deposit." << endl << "$"; 
+	getline (cin, input); 
+	fDeposit = std::stof(RemoveGarbage(input));
+	
+	//Ask user for input, then remove any commas or spaces from it using function RemoveGarbage
+	cout << "How much will you deposit per month?" << endl << "&";
+	getline (cin, input);
+	monthDeposit = std::stof(RemoveGarbage(input));
+	
+	//Ask user for input, then remove any commas or spaces from it using function RemoveGarbage
 	cout << "What is the yearly interest rate on the account?" << endl;
 	cout << "%";
-	cin >> rawRate;
+	cin >> rate;
 	
-	rate = rawRate/100; //converting interest rate from percent to decimal
+	rate = rate/100; //converting interest rate from percent to decimal
 	balance=fDeposit; //setting starting balance to equal the amount of the first deposit
 	interestGain=CalcIntMonth(balance, monthDeposit, rate); //Invoking function to update balance and calculate interest earned
-
-	//Displaying ending balance, total deposits, and total interests all set at 2 decimal places.
+	
+	//convert balance to String to allow formatting as currency using function FormatCurrency
+	output=to_string (balance); 
+	FormatCurrency(output);
+	
+	//Displaying ending balance, using function Rounding to set to 2 decimal places.
 	cout << "The balance in the account after one year is $";
-	printf("%.2f", balance);
+	cout << Rounding(output);
 	cout << endl;
 	
+	
+	//convert total amount of money deposited by user to String to allow formatting as currency using function FormatCurrency
+	output= to_string(fDeposit+(monthDeposit*11));
+	FormatCurrency(output);
+	
+	//Displaying total money deposited by user, using function Rounding to set to 2 decimal places.
 	cout << "The amount of money you deposited into the account is $";
-	printf("%.2f", (fDeposit+(monthDeposit*11)));
+	cout << Rounding(output);
 	cout << endl;
 	
-	cout << "The total amount of interest gained is $";
-	printf("%.2f", interestGain);
+	//convert total amount of interest earned to String to allow formatting as currency using function FormatCurrency
+	output = to_string(interestGain);
+	FormatCurrency(output);
 	
+	//Displaying total interest earned, using function Rounding to set to 2 decimal places.
+	cout << "The total amount of interest gained is $";
+	cout << Rounding(output);
 
 return 0;
 }
